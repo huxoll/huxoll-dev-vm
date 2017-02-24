5a43344bc3071bb252bcb60f5fba1c29958a7397
John Gardner <gardnerj@vmware.com>
2017-02-24 16:07:25 -0600

    Use template files; better shell use.

--- dev.sh
+++ dev.sh
@@ -9,10 +9,81 @@ usage() {
 }
 
 run_docker_platform() {
-    cd platforms/${platform}
-    docker-compose up
-    docker-compose run ${platform} /bin/bash
+  # Suck in platform config and expose those as variables like "config_yamlvar"
+  eval $(parse_yaml platforms/$platform.yml "config_")
+  [[ -n ${verbose} ]] && echo "config_from: $config_from"
+  tmp_dir=`mktemp -d`
+  fill_template < platforms/docker-compose-template.yml > ${tmp_dir}/docker-compose.yml
+  fill_template < platforms/Dockerfile-template > ${work_dir}/Dockerfile.dev
+
+  [[ -n ${verbose} ]] && echo "docker compose file: ${tmp_dir}/docker-compose.yml"
+
+  #cd $work_dir
+  docker-compose -f ${tmp_dir}/docker-compose.yml build
+  compose_out=$(docker-compose -f ${tmp_dir}/docker-compose.yml up -d 2>&1)
+  if [[ $? -ne 0 ]]; then
+    echo "Docker compose failed!"
+    echo "${compose_out}"
+    exit 1
+  fi
+  [[ -n ${verbose} ]] && echo "COMPOSE_OUT: ${compose_out} ##"
+  built=0
+  container=''
+  while read -r line
+  do
+    if [[ "${built}" == "0" ]]; then
+      built=$(echo $line | grep -ce "Creating .*$platform")
+    fi
+    if [[ "${built}" > '0' ]]; then
+      echo "Built docker images."
+    fi
+    regex="Creating (.*$platform.*)"
+    if [[ "${line}" =~ ${regex} ]]; then
+      container="${BASH_REMATCH[1]}"
+      echo "Got container: ${container}"
+    fi
+  done <<< "${compose_out}"
+  docker exec -ti ${container} /bin/bash
+  echo "All done: ${container}"
+  docker-compose -f ${tmp_dir}/docker-compose.yml down
 }
+
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+fill_template() {
+  IFS=''
+  while read -r line ; do
+      while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
+          LHS=${BASH_REMATCH[1]}
+          RHS="$(eval echo "\"$LHS\"")"
+          line=${line//$LHS/$RHS}
+      done
+      echo "$line"
+  done
+  unset IFS
+}
+
+dir_resolve()
+{
+  cd "$1" 2>/dev/null || return $?
+  echo "`pwd -P`"
+}
+
 
 # Initialize our own variables:
 platform=""
@@ -63,9 +134,13 @@ fi
 
 if [[ -n $git_url ]]; then
     echo "Checkout out source from ${git_url} to ${work_dir}..."
-    git clone -q ${git_url} ${work_dir}
+    git clone -q ${git_url} ${work_dir} || exit 1
 fi
 
+[[ -n ${verbose} ]] && echo "Using work directory '$work_dir'."
+[[ ! -d ${work_dir} ]] && echo "No such directory '$work_dir'." && exit 1
+work_dir=`( dir_resolve $work_dir )`
+[[ -n ${verbose} ]] && echo "Using work directory '$work_dir'."
 
 # Execute docker
 pushd . >> /dev/null
