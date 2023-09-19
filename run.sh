5d4604121c5b4fe62c287d5b2124f929eef889cb
John Gardner <huxoll@gmail.com>
2023-09-19 15:50:01 -0500

    Add rudimentary run.sh, add gnu parallels, webp.

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-ansible-playbook -i inventory --extra-vars "@extra_vars.yml" dev-vm.yaml -K
+ARGS=""
+
+while getopts "hqvd" arg; do
+    case "${arg}" in
+        h)      echo >&2 "Usage: $0 [-h]..."
+                exit 1;;
+        q)
+            ARGS="-q $ARGS"
+            ;;
+        v)
+            ARGS="$ARGS -vv"
+            ;;
+        d)
+            ARGS="$ARGS --check"
+            ;;
+    esac
+done
+
+ansible-playbook -i inventory --extra-vars "@extra_vars.yml" dev-vm.yaml -K $ARGS
